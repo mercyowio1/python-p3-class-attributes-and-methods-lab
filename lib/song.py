e61eeeefe95bd3e343beed0986d5a16ae1ea89b5
Mercy <owinomercy983@gmail.com>
2025-05-22 08:29:25 +0300

    method lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,30 @@
 class Song:
-    pass
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        # Update total count
+        Song.count += 1
+        
+        # Add to genres and artists sets
+        Song.genres.add(genre)
+        Song.artists.add(artist)
+        
+        # Update genre_count dictionary
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+        
+        # Update artist_count dictionary
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
